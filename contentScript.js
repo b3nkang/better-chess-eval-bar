6cd82babd39be91b84fa75902af03c06e2aeaa6a
BKtheInfamous <134336066+BKtheInfamous@users.noreply.github.com>
2023-08-13 01:05:13 -0700

    configure bar overlays properly, target moveNodes and moveTurn fulfilled

--- contentScript.js
+++ contentScript.js
@@ -1,21 +1,11 @@
-alert("contentScript entrance");
+//alert("contentScript entrance");
 
 setTimeout(evalInjection,  5000);  
 
 function evalInjection() {
     const whiteBar = document.querySelector('.evaluation-bar-white'); // white bar main class (unchanged)
-    const blackBar = document.querySelector('.evaluation-bar-black'); // black bar class
-    const drawBar = document.querySelector('.evaluation-bar-draw'); // draw bar class
-
-
-    /**    const whiteBar = document.querySelector('.evaluation-bar-white'); // white bar main class (unchanged)
-    const blackBar = document.querySelector('.evaluation-bar-black'); // black bar class
+    const white2ndBar = document.querySelector('.evaluation-bar-black'); // black bar class
     const black2ndBar = document.querySelector('.evaluation-bar-draw'); // draw bar class
- */
-    whiteBar.style.cssText = "background-color: red";
-    blackBar.style.cssText = "background-color: blue";
-    drawBar.style.cssText = "background-color: green";
-
 
     let eval = get2ndLine();
     height = getEvalHeight(eval);
@@ -24,9 +14,9 @@ function evalInjection() {
 
     let height2 = 100-height;
 
-    whiteBar.style.cssText = `background-color: grey; transform: translate3d(0px, ${height}%, 0px);`;
-    drawBar.style.cssText = `background-color: red; transform: translate3d(0px, ${height2*(1/3)}%, 0px);`;
-    blackBar.style.cssText = `background-color: yellow; transform: translate3d(0px, ${height2 *(2/3)}%, 0px);`;
+    whiteBar.style.cssText = `background-color: rgb(205, 205, 205); transform: translate3d(0px, ${height}%, 0px);`;
+    white2ndBar.style.cssText = `background-color: rgb(250,250,250); transform: translate3d(0px, ${height2 *(2/3)}%, 0px);`;
+    black2ndBar.style.cssText = `background-color: rgb(80,80,80); transform: translate3d(0px, ${height2*(1/3)}%, 0px);`;
     console.log("Draw bar height %age: "+height2*(1/3));
     console.log("Black bar height %age: "+height2*(2/3));
 
@@ -35,14 +25,6 @@ function evalInjection() {
     - white bar overwrites all others
     - black bar overwrites draw bar
     - draw bar is the lowest */
-    /* let greyBar = document.createElement('div');
-    greyBar.style.cssText = `background-color: grey; height: ${height}%; transform: translate3d(0px, ${height}%, 0px);`;
-    blueBar.style.cssText = "background-color: blue; height: 5%; transform: translate3d(0px, 28.325%, 0px);"
-    while (whiteBar.firstChild) {
-        whiteBar.removeChild(whiteBar.firstChild);
-    }
-    whiteBar.appendChild(greyBar);
-    whiteBar.appendChild(blueBar);*/
 }
 
 function getEvalHeight(eval){
@@ -64,10 +46,50 @@ function get2ndLine() {
     let secondEvalStr = "";
     if (lines.length >= 2) {
        secondEvalStr = lines[1].textContent;
+       let moveNode = lines[1].nextElementSibling;
+       let moveNodeText = moveNode.textContent;
+       console.log("Move text: " +moveNodeText);
+       let moveTurn = checkPeriods(moveNodeText);
+
+       console.log("Move turn: "+moveTurn);
        console.log(secondEvalStr);
+
        let secondEval = parseFloat(secondEvalStr);
        return secondEval;
     } else {
         console.log("Not enough elements with the specified class.");
     }
 }  
+
+function getMoveTurn(input) {
+    const match = input.match(/^(\d+)(\.|\.{3}).*/);
+    
+    if (match) {
+        const numberOfPeriods = match[2].length;
+        
+        if (numberOfPeriods === 1) {
+            return "white";
+        } else if (numberOfPeriods === 3) {
+            return "black";
+        }
+    }
+    
+    return "invalid move";
+}
+
+function checkPeriods(str) {
+    const onePeriodRegex = /\./g;
+    const threePeriodsRegex = /\.{3}/g;
+  
+    const onePeriodMatches = (str.match(onePeriodRegex) || []).length;
+    const threePeriodsMatches = (str.match(threePeriodsRegex) || []).length;
+  
+    if (onePeriodMatches === 1) {
+      return "white";
+    } else if (threePeriodsMatches === 1) {
+      return "black";
+    } else {
+      return "unknown";
+    }
+  }
+  
